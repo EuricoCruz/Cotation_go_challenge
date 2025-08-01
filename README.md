451980f1f474d64c09dd682c2228b69157b4ce89
@esouza <esouza@palo-it.com>
2025-07-31 21:19:16 -0300

    finish server.go

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Cotation_go_challenge
 Desafio da pós go-expert
+
+rodar o server
+```bash
+cd server/cmd
+go run main.go
+``` 
+
+
--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	_ "github.com/mattn/go-sqlite3"
+	"time"
 
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Cotacao struct {
+	Bid string `json:"bid"`
+}
+
+type ResponseAPI struct {
+	USDBRL Cotacao `json:"USDBRL"`
+}
+
+var db *sql.DB
+var err error
 
 
 func main() {
-
-//	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
-
-	db, err := sql.Open("sqlite3", "./cotation.db")
+	
+	db, err = sql.Open("sqlite3", "./cotation.db")
 	if err != nil {
 		panic(err)
 	}
@@ -35,11 +47,47 @@ func main() {
 }
 
 func handleCotation(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Fprintf(w, "Cotation endpoint is under construction")
+	url := "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+	ctx, cancel := context.WithTimeout(r.Context(),  200 * time.Millisecond)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		http.Error(w, "Failed to create request", http.StatusInternalServerError)
+		return
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		http.Error(w, "Erro ao obter cotação", http.StatusGatewayTimeout)
+		fmt.Println("Erro na requisição externa:", err)
+		return
+	}
+
+	var apiResp ResponseAPI
+	if err := json.NewDecoder(res.Body).Decode(&apiResp); err != nil {
+		http.Error(w, "Erro ao decodificar resposta", http.StatusInternalServerError)
+		fmt.Println("Erro ao decodificar JSON:", err)
+		return
+	}
+	fmt.Println("Cotação obtida:", apiResp.USDBRL.Bid)
+	defer res.Body.Close()
+
+	ctxDB, cancelDB := context.WithTimeout(r.Context(), 10 * time.Millisecond)
+	defer cancelDB()
+	err = saveCotacao(ctxDB, apiResp.USDBRL.Bid)
+	if err != nil {
+		fmt.Println("Erro ao salvar no banco:", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"bid": apiResp.USDBRL.Bid})
+	
 }
 
 func createTable(db *sql.DB) {
@@ -51,4 +99,15 @@ func createTable(db *sql.DB) {
 	if err != nil {
 		fmt.Printf("Erro ao criar tabela: %v", err)
 	}
+}
+
+func saveCotacao(ctx context.Context, bid string) error {
+	stmt, err := db.PrepareContext(ctx, "INSERT INTO cotacoes(bid) VALUES(?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, bid)
+	return err
 }
